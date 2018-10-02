9c4390e1d5c25c857590906d49f3823e2ff09ab2
Tushar Sharma <ts16sharma@gmail.com>
2018-10-03 02:11:14 +0530

    Add files via upload

---
+++ src/project/Display.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project;
+
+/**
+ *
+ * @author USER
+ */
+class Display {
+
+    void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void sleep() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean readAndDispatch() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
